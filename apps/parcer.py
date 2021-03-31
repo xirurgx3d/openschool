65d792e21d856e34840d88e73dc3e2b780a7f396
Ваше Имя <you@example.com>
2021-03-31 15:50:07 -0300

    parser start

---
+++ apps/parcer.py
@@ -0,0 +1,23 @@
+import parcer_app.init as Init
+
+
+# Инициализировать драйвер
+driver = Init.Driver.login
+
+
+# Тест Работы
+driver.get("http://www.wikipedia.org")
+driver.close()
+
+
+
+# Выбор - Выберите что парсить
+
+
+# Инпут - Введите путь к исходному файлу
+
+
+# Файл не наден
+
+
+# 
